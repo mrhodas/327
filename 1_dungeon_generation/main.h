51f427c16b207914f29d674e6e88ac23cc940305
Michael Rhodas <mjrhodas@iastate.edu>
2016-09-06 23:16:58 -0500

    dungeon generation creation and start

---
+++ 1_dungeon_generation/main.h
@@ -0,0 +1,11 @@
+//
+// Created by Michael Rhodas on 9/6/16.
+//
+
+#ifndef ASSIGNMENT1_MAIN_H
+#define ASSIGNMENT1_MAIN_H
+
+#endif //ASSIGNMENT1_MAIN_H
+
+void printMap(char** map);
+void generateDungeon();
