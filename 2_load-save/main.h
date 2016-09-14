b73aefd3049cbcc4cc9a1602f2d51a09b4d28989
mjrhodas <10755562mike@gmail.com>
2016-09-13 23:08:58 -0500

    ass 2 start

---
+++ 2_load-save/main.h
@@ -0,0 +1,14 @@
+//
+// Created by Michael Rhodas on 9/6/16.
+//
+
+#ifndef ASSIGNMENT1_MAIN_H
+#define ASSIGNMENT1_MAIN_H
+
+#endif //ASSIGNMENT1_MAIN_H
+
+void printMap(char** map);
+void generateDungeon();
+void addRoom(int x, int y, int width, int height);
+int checkPlacement(int x, int y, int width, int height);
+void drawCorridors(int* roomList, int numberOfRooms);
